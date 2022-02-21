cc12cfcfd4602145ba86ab0be90d245a72a1b467
Jonathen Adsit <adsit@ualberta.ca>
2022-02-21 15:47:15 -0700

    DatabaseManager created
    App is properly connected to FireStore.
    Created a DatabaseManager class for reading and writing to FireStore.

---
+++ QuiRky/app/src/main/java/com/example/quirky/DatabaseManager.java
@@ -0,0 +1,123 @@
+package com.example.quirky;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+DatabaseManager:
+    This class manages all writing and reading from our database at FireStore.
+    Create an instance of this class whenever you want to write or read from the database
+
+    Our Database works in the following way:
+        The database has Collections.
+            Collections are like directories.
+        The Collections have Documents
+            Documents store the data.
+        Documents store data in field-value pairs, like a dictionary
+
+        The Hierarchy is:
+        Database
+            Collections
+                Documents
+                    Data (Key-Value Pairs)
+
+    To use the DatabaseManager:
+        Create an instance of the class
+            - DatabaseManager dm = new DatabaseManager();
+        Set the collection you want to write to
+            - dm.setCollection("users");
+        Create a Map and fill it with data
+            - Map<String, String> data = new HashMap();
+            - data.put("key, "value");
+        Use the write method
+            - dm.write(data);
+     This method will create a new document every time you write.
+ */
+public class DatabaseManager {
+    private FirebaseFirestore db;
+    private CollectionReference col_ref;
+    private DocumentReference doc_ref;
+
+    private OnSuccessListener<DocumentReference> sl;
+    private OnFailureListener fl;
+
+    private String TAG = "Sample";
+
+    public DatabaseManager() {
+        db = FirebaseFirestore.getInstance();
+        sl = docref -> Log.d("Sample", "DocumentSnapshot added with ID: " + docref.getId());
+        fl = e -> Log.d("Sample", "Error adding document! ", e);
+
+        col_ref = db.collection("users");
+    }
+
+    public DatabaseManager(String collection) {
+        db = FirebaseFirestore.getInstance();
+        sl = docref -> Log.d(TAG, "DocumentSnapshot added with ID: " + docref.getId());
+        fl = e -> Log.d(TAG, "Error adding document! ", e);
+
+        col_ref = db.collection(collection);
+    }
+
+    public void setCollection(String collection) {
+        col_ref = db.collection((collection));
+    }
+
+    public void setDoc(String doc) {
+        doc_ref = col_ref.document(doc);
+    }
+
+    public void write(Map<String, Object> data) {
+        col_ref.add(data).addOnSuccessListener(sl).addOnFailureListener(fl);
+    }
+
+
+    // FIXME: Currently have two read methods: read() and readAlt(), implementing two seperate methods of reading from the database
+    public Map<String, Object> read(String doc) {
+        Map<String, Object> data;
+
+        col_ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for(QueryDocumentSnapshot doc : task.getResult()) {
+                        Log.d(TAG, doc.getId() + " => " + doc.getData());
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
+            }
+        });
+
+        // FIXME: this read method currently does not work.
+        data = new HashMap<>();
+        return data;
+    }
+
+    public Map<String, Object> readAlt(String doc) {
+        Task<DocumentSnapshot> t;
+        t = col_ref.document(doc).get();
+
+        // FIXME: The Task object represents a task.
+        // FIXME:   Calling col_ref.document(doc).get() will start a task that will take time to complete
+        // FIXME:   Calling t.getResult() immediately may cause bugs if the task is not yet complete
+        // FIXME:   Must implement some kind of stalling feature?
+
+        return t.getResult().getData(); // TODO: t.getResult().getData() can return Null. Make sure this case is accounted for.
+    }
+}
--- QuiRky/app/src/main/java/com/example/quirky/MainActivity.java
+++ QuiRky/app/src/main/java/com/example/quirky/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseManager dm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "JJ");
+        user.put("last", "A.");
+        user.put("year", "1887");
+
+        dm = new DatabaseManager();
+
     }
 }
