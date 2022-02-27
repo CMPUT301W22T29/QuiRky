3e36366e6297fe3169c73cb3ee6f6e14b342f5e5
jJadsit <adsit@ualberta.ca>
2022-02-27 11:03:01 -0700

    Jonathen's Bugfixing
    Buxfixing while reviewing this branch. This commit should remain local to my computer, if it goes remote, I have done something wrong.

--- QuiRky/app/src/main/java/com/example/quirky/Setting.java
+++ QuiRky/app/src/main/java/com/example/quirky/Setting.java
@@ -2,21 +2,17 @@ package com.example.quirky;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 /*
  * Main Activity class that loads {@link MainFragment}.
  */
-public class Setting extends FragmentActivity {
+public class Setting extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_browse_fragment, new MainFragment())
-                    .commitNow();
-        }
     }
 }
