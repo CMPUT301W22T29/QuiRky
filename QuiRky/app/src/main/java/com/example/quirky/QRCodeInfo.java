ee36b60abd376a70691e78360e48ddfedafc47a6
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2022-03-07 17:11:38 -0700

    Made some QRCodeInfo and Comment Class. Also made QRCodeInfo Activity
    This is so that there is a way to access the comment class. QRCodeInfo will not have many functionalities yet, but it will prolly just be leading to other activities, in which I can add later on.
    
    QR code Selected is just a place holder at the moment. I still need to add a back button.
    
    Main Goal is to Get comment class working properly then anything of the activities that surrounds it made and leads to comment activity and maybe some of its functionality working.
    
    The design is based on the project part 2 Miro "Comment" design

---
+++ QuiRky/app/src/main/java/com/example/quirky/QRCodeInfo.java
@@ -0,0 +1,9 @@
+package com.example.quirky;
+
+/**
+ * This class is of the Activity for when you're clicking on one of the listed items in "Your QR Codes" or
+ * "nearby QR code" in "MAP" Activity, as seen on the Project Part 2 Miro.
+ * */
+public class QRCodeInfo {
+
+}
