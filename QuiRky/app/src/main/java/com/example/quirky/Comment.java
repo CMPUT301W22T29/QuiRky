adce9bcb9acee7736a711274e7c441ccb5558505
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2022-03-07 20:44:00 -0700

    Working on the Comment class
    Partially complete. Got the Activity done.

--- QuiRky/app/src/main/java/com/example/quirky/Comment.java
+++ QuiRky/app/src/main/java/com/example/quirky/Comment.java
@@ -1,5 +1,34 @@
 package com.example.quirky;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class Comment {
+    private Button Save;
+    private Button Cancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialized intent
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancel();
+            }
+        });
+
+        // How to write a comment section
+    }
+
 
 }
