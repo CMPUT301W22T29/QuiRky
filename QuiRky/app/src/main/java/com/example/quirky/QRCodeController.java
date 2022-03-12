ec433e353a12c6fde293870b36e9017465338162
Sean Meyers <smmeyers@ualberta.ca>
2022-03-12 16:13:51 -0700

    Added barcode options field to QRCodeController class

--- QuiRky/app/src/main/java/com/example/quirky/QRCodeController.java
+++ QuiRky/app/src/main/java/com/example/quirky/QRCodeController.java
@@ -1,6 +1,11 @@
 package com.example.quirky;
 
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.common.Barcode;
+
 public class QRCodeController {
+    private static final BarcodeScannerOptions barcodeOptions =
+            new BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build();
     // TODO: Everything.
 
     // TODO: get qr code stuff from input image ();
