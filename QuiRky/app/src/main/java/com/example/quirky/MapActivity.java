7177ed75735587304d372cee362dd912f4088e58
HengYuan Zhang <he12@ualberta.ca>
2022-03-19 12:24:14 -0600

    Loading

--- QuiRky/app/src/main/java/com/example/quirky/MapActivity.java
+++ QuiRky/app/src/main/java/com/example/quirky/MapActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -17,6 +20,7 @@ import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 
 /*
 source: https://osmdroid.github.io/osmdroid/How-to-use-the-osmdroid-library.html
@@ -29,6 +33,7 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
        
         super.onCreate(savedInstanceState);
+
 
         //Initialize the osmdroid configuration which can be done through
         Context cxt = getApplicationContext();
@@ -44,26 +49,33 @@ public class MapActivity extends AppCompatActivity {
         IMapController mapController = nearbymap.getController();
         mapController.setZoom(15);
         //zoom into university of Alberta
-        GeoPoint startPoint = new GeoPoint(53.52682, -113.52449);
+        //Get current location automatically
+        GpsMyLocationProvider provider = new GpsMyLocationProvider(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationManager mLocMgr = null;
+        Location location = mLocMgr.getLastKnownLocation(String.valueOf(provider));
+        GeoPoint startPoint = new GeoPoint(location.getLatitude(),location.getLongitude());
         mapController.setCenter(startPoint);
-        //set a marker on our current location
         Marker qrmarker = new Marker(nearbymap);
         qrmarker.setPosition(startPoint);
         qrmarker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
         nearbymap.getOverlays().add(qrmarker);
         qrmarker.setTitle("Current location");
-        // use this to assign QR codes images to our marker
-        // qrmarker.setImage();
-        /*qrmarker.setOnMarkerClickListener(final Marker.OnMarkerClickListener listener){
-            @Override
-            public boolean onMarkerClick(Marker marker){
-                if(marker.equals(qrmarker)){
-                    qrmarker.setTitle("Current location");
-                    return true;
-                }
-                return false;
-            }
-        }*/
+        //provider.addLocationSource(LocationManager.NETWORK_PROVIDER);
+        //locationOverlay = new MyLocationNewOverlay(provider, nearbymap);
+        //locationOverlay.enableFollowLocation();
+        //nearbymap.getOverlayManager().add(locationOverlay);
+        //set a marker on our current location
+
 
 
     }
