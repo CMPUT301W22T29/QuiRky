99469bd1a702eb611b62c066b8f359e0a8ce7d10
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2022-03-19 22:09:35 -0600

    Fragment Crashes, also laptop slow will transfer work to pc.
    Fragment Crashes the whole thing, so I need to fix my fragment so that it launches properly

--- QuiRky/app/src/main/java/com/example/quirky/ViewQRActivity.java
+++ QuiRky/app/src/main/java/com/example/quirky/ViewQRActivity.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.FragmentActivity;
  * This class is of the Activity for when you're clicking on one of the listed items in "Your QR Codes" or
  * "nearby QR code" in "MAP" Activity, as seen on the Project Part 2 Miro.
  * */
-public class ViewQRActivity extends FragmentActivity {
+public class ViewQRActivity extends AppCompatActivity{
 
     private ViewQRFragment optionFragment;
 
