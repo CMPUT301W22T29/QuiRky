8f2b27320a662450cf18498cbeb118a11a2c79ee
HengYuan Zhang <he12@ualberta.ca>
2022-03-20 16:56:27 -0600

    Almost done map

--- QuiRky/app/src/main/java/com/example/quirky/MapActivity.java
+++ QuiRky/app/src/main/java/com/example/quirky/MapActivity.java
@@ -1,14 +1,16 @@
 package com.example.quirky;
 
-import static androidx.camera.core.CameraX.getContext;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.Manifest;
+
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -21,15 +23,10 @@ import androidx.core.content.ContextCompat;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;//Tile source factory used for manipulating the map
-import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.Overlay;
-import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /*
 source: https://osmdroid.github.io/osmdroid/How-to-use-the-osmdroid-library.html
@@ -41,25 +38,44 @@ public class MapActivity extends AppCompatActivity {
     MapView nearbymap = null;
     private MyLocationNewOverlay locationOverlay;
     private LocationManager mLocMgr;
-    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+    private static final String[] LOCATION_PERMISSION = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+    public static final int LOCATION_REQUEST_CODE = 99;
+    private Object showExplanation;
 
 
-
+    /*private void ShowLocationStatus() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            }
+        } else {
+            Toast.makeText(MapActivity.this, "Permission is Granted", Toast.LENGTH_SHORT).show();
+        }
+    }*/
 
     @Override
-
-
     protected void onCreate(Bundle savedInstanceState) {
-       
         super.onCreate(savedInstanceState);
-
-
         //Initialize the osmdroid configuration which can be done through
         Context cxt = getApplicationContext();
         Configuration.getInstance().load(cxt, PreferenceManager.getDefaultSharedPreferences(cxt));
 
         //Create a map
         setContentView(R.layout.activity_map_layout);
+        GpsMyLocationProvider provider = new GpsMyLocationProvider(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST_CODE);
+            return;
+        } else {
+            Toast.makeText(MapActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+        Location location = mLocMgr.requestLocationUpdates(provider,5000,5,this);
+        setmap(location);
+    }
+    public void setmap(Location location) {
         nearbymap = (MapView) findViewById(R.id.map);
         nearbymap.setTileSource(TileSourceFactory.MAPNIK);
         //Make the map can zoom in or out
@@ -67,20 +83,7 @@ public class MapActivity extends AppCompatActivity {
         nearbymap.setMultiTouchControls(true);
         IMapController mapController = nearbymap.getController();
         mapController.setZoom(15);
-        //zoom into university of Alberta
         //Get current location automatically
-        GpsMyLocationProvider provider = new GpsMyLocationProvider(this);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location location = mLocMgr.getLastKnownLocation(String.valueOf(provider));
         GeoPoint startPoint = new GeoPoint(location.getLatitude(),location.getLongitude());
         mapController.setCenter(startPoint);
         Marker qrmarker = new Marker(nearbymap);
@@ -93,14 +96,21 @@ public class MapActivity extends AppCompatActivity {
         //locationOverlay.enableFollowLocation();
         //nearbymap.getOverlayManager().add(locationOverlay);
         //set a marker on our current location
-
-
-
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MapActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
 
-
-
+                } else{
+                    Toast.makeText(MapActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
 
     public void onResume(){
         super.onResume();
