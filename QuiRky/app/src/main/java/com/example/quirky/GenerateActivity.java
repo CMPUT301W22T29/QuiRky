bf1f1618dd9ce1e4d2f08560a852fbecc92e733a
jJadsit <adsit@ualberta.ca>
2022-04-03 12:13:33 -0600

    Generate Activity Update to be functional

--- QuiRky/app/src/main/java/com/example/quirky/GenerateActivity.java
+++ QuiRky/app/src/main/java/com/example/quirky/GenerateActivity.java
@@ -1,55 +1,62 @@
 package com.example.quirky;
 
-import static com.example.quirky.QRCodeController.generateQR;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
-
-import java.util.Random;
+import android.widget.Toast;
 
 public class GenerateActivity extends AppCompatActivity {
 
-    Button qrGeneraBtn1, getBack;
+    Button generateLogin, generateViewProfile, generateText;
+    EditText inputField;
     ImageView qrImage;
+
+    String user;
+    DatabaseController dc;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate);
-        qrGeneraBtn1 = findViewById(R.id.generateButton1);
+
+        generateLogin = findViewById(R.id.generateLoginCodeButton);
+        generateViewProfile = findViewById(R.id.GenerateViewProfileCodeButton);
+        generateText = findViewById(R.id.generateByTextButton);
+        inputField = findViewById(R.id.genertateByTextField);
         qrImage = findViewById(R.id.qrfield);
 
+        dc = new DatabaseController(this);
+        user = new MemoryController(this).readUser();
 
-        qrGeneraBtn1.setOnClickListener(v->{
-            Bitmap generated =null;
-            generated = QRCodeController.generateQR(QRCodeController.getRandomString(18));
+        generateLogin.setOnClickListener(v -> {
+            String loginPassword = QRCodeController.getRandomString(15);
+
+            Bitmap generated = QRCodeController.generateQR(loginPassword);
+            qrImage.setImageBitmap(generated);
+
+            String hashedPassword = QRCodeController.SHA256(loginPassword);
+            dc.writeLoginHash(hashedPassword, user);
+
+            Toast.makeText(this, "Save this code! You will need it to login again!", Toast.LENGTH_LONG).show();
+        });
+
+        generateText.setOnClickListener(v -> {
+            String text = inputField.getText().toString();
+            if (text.length() > 0) {
+                Bitmap generated = QRCodeController.generateQR(text);
+                qrImage.setImageBitmap(generated);
+            }
+        });
+
+        generateViewProfile.setOnClickListener(v -> {
+            String text = "quirky.view.profile." + user;
+            Bitmap generated = QRCodeController.generateQR(text);
             qrImage.setImageBitmap(generated);
         });
-        getBack = findViewById(R.id.randomGenerBack);
-        getBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                returnToPre();
-            }
-    });
-    }
-    public void returnToPre() { //to previous page
-        Intent intent1 = new Intent(this, StartingPageActivity.class);
-        startActivity(intent1);
     }
 }
