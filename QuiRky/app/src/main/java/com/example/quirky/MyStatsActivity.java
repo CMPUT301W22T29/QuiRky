f9993df4a461fb98a0ae379bc04a5d8c08d74c8d
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2022-04-03 16:18:19 -0600

    Assuming that the bug is only occurring because I don't have any scanned QR Codes, then this Activity should be good..
    Implemented a default message to be displayed if you no scanned QR codes

--- QuiRky/app/src/main/java/com/example/quirky/MyStatsActivity.java
+++ QuiRky/app/src/main/java/com/example/quirky/MyStatsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyStatsActivity extends AppCompatActivity {
     private Button backBt;
@@ -21,19 +22,26 @@ public class MyStatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_stats);
 
-        // Do I need to get the profile?
         i = getIntent();
         p = (Profile) i.getSerializableExtra("profile");
         lbc = new LeaderBoardController(this);
-        lbc.readAllPlayers().addOnCompleteListener(task -> setTextRanking());
 
         totScore = findViewById(R.id.totScore2);
         scoreRanking = findViewById(R.id.scoreRanking2);
         totScanned = findViewById(R.id.totScanned2);
         scannedRanking = findViewById(R.id.scannedRanking2);
 
-        totScore.setText(String.valueOf(p.getPointsOfScannedCodes()));
-        totScanned.setText(String.valueOf(p.getNumberCodesScanned()));
+        int numTotScore = p.getPointsOfScannedCodes();
+        int numTotScanned = p.getNumberCodesScanned();
+
+        System.out.println(p.getNumberCodesScanned());
+        Toast.makeText(this, String.valueOf(p.getNumberCodesScanned()), Toast.LENGTH_LONG).show();
+        if (numTotScore < 1 && numTotScanned < 1) {
+            defaultValues();
+        }
+        else {
+            replaceValues();
+        }
 
         backBt = (Button)findViewById(R.id.mystatsBack);
         backBt.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +51,19 @@ public class MyStatsActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void defaultValues() {
+        int zeroVal = 0;
+        totScore.setText(String.valueOf(zeroVal));
+        totScanned.setText(String.valueOf(zeroVal));
+        scoreRanking.setText(String.valueOf(zeroVal));
+        scannedRanking.setText(String.valueOf(zeroVal));
+    }
+
+    private void replaceValues() {
+        totScore.setText(String.valueOf(p.getPointsOfScannedCodes()));
+        totScanned.setText(String.valueOf(p.getNumberCodesScanned()));
+        lbc.readAllPlayers().addOnCompleteListener(task -> setTextRanking());
     }
 
     private void setTextRanking(){
